4bb14a3b75e21be08e6c0c66dea373afe2bbaf89
yusuf180 <95351352+yusuf180@users.noreply.github.com>
2023-05-22 13:43:42 +0300

    building ui and some minor changes

--- app/src/main/java/com/yusufguler/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/yusufguler/todoapp/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.yusufguler.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.yusufguler.todoapp.R
 
 
@@ -14,8 +12,12 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
+    }
 }
--- app/src/main/java/com/yusufguler/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/yusufguler/todoapp/fragments/list/ListFragment.kt
@@ -1,20 +1,39 @@
 package com.yusufguler.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.yusufguler.todoapp.R
+import com.yusufguler.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+        //set menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
